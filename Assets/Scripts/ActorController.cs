607a5c33a25d7b82883bde6f611c8283b8a05090
Nagisa3113 <nagisa3113@gmail.com>
2022-03-30 13:00:23 +0800

    L26 animation state

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    public GameObject model;
+    public PlayerInput pi;
+    public float walkSpeed = 2.0f;
+    public float runMultiplier = 2.0f;
+    public float jumpVelocity;
+    public float rollVelocity = 1f;
+
+    [SerializeField] private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 planarVec;
+    private Vector3 thrustVec;
+
+    public bool lockPlanar = false;
+
+    void Awake()
+    {
+        pi = GetComponent<PlayerInput>();
+        anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("forward", pi.Dmag * Mathf.Lerp(anim.GetFloat("forward"), ((pi.run) ? 2.0f : 1.0f), 0.5f));
+
+        if (rigid.velocity.magnitude > 1.0f)
+        {
+            anim.SetTrigger("roll");
+        }
+
+
+        if (pi.jump)
+        {
+            anim.SetTrigger("jump");
+        }
+
+        if (pi.Dmag > 0.1f)
+        {
+            model.transform.forward = Vector3.Slerp(model.transform.forward, pi.Dvec, 0.3f);
+        }
+
+        if (lockPlanar == false)
+        {
+            planarVec = pi.Dmag * model.transform.forward * walkSpeed * (pi.run ? runMultiplier : 1.0f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // rigid.position += movingVec * Time.fixedDeltaTime;
+        rigid.velocity = new Vector3(planarVec.x, rigid.velocity.y, planarVec.z) + thrustVec;
+        thrustVec = Vector3.zero;
+    }
+
+
+    /// <summary>
+    /// Message processing block
+    /// </summary>
+    public void OnJumpEnter()
+    {
+        thrustVec = new Vector3(0, jumpVelocity, 0);
+        pi.inputEnabled = false;
+        lockPlanar = true;
+    }
+
+    public void OnJumpExit()
+    {
+        pi.inputEnabled = true;
+        lockPlanar = false;
+    }
+
+    public void IsGround()
+    {
+        anim.SetBool("isGround", true);
+    }
+
+    public void IsNotGround()
+    {
+        anim.SetBool("isGround", false);
+    }
+
+    public void OnGroundEnter()
+    {
+        pi.inputEnabled = true;
+        lockPlanar = false;
+    }
+
+    public void OnFallEnter()
+    {
+        pi.inputEnabled = false;
+        lockPlanar = true;
+    }
+
+    public void OnRollEnter()
+    {
+        thrustVec = new Vector3(0, rollVelocity, 0);
+        pi.inputEnabled = false;
+        lockPlanar = true;
+    }
+
+    public void OnJabEnter()
+    {
+        pi.inputEnabled = false;
+        lockPlanar = true;
+    }
+
+    public void OnJabUpdate()
+    {
+        thrustVec = model.transform.forward * anim.GetFloat("jabVelocity");
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    //Variable
+    [Header("==== Key settings ====")] public string keyUp;
+    public string keyDown;
+    public string keyLeft;
+    public string keyRight;
+
+    public string keyA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("==== Output Signals ====")] public float Dup;
+    public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
+
+    // 1. pressing signal
+    public bool run;
+
+    // 2. trigger once signal
+    public bool jump;
+
+    public bool lastJump;
+    // 3. double trigger
+
+
+    [Header("==== Other settings ====")] public bool inputEnabled = true;
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+
+        if (inputEnabled == false)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+        Vector2 tempDAxis = SquareToCircle(new Vector2(Dright, Dup));
+
+        float Dright2 = tempDAxis.x;
+        float Dup2 = tempDAxis.y;
+
+        Dmag = Mathf.Sqrt((Dup2 * Dup2) + (Dright2 * Dright2));
+        Dvec = Dright2 * transform.right + Dup2 * transform.forward;
+
+        run = Input.GetKey(keyA);
+
+        bool newJump = Input.GetKey(keyB);
+        jump = newJump;
+        if (newJump != lastJump && newJump == true)
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+
+        lastJump = newJump;
+    }
+
+    //Mapping for Squaring the Circular Disc
+    //arxiv.org/1509/1509.06344
+    private Vector2 SquareToCircle(Vector2 input)
+    {
+        Vector2 output = Vector2.zero;
+
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f);
+
+        return output;
+    }
+}
